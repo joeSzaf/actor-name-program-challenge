c8a6738ce0b16a423d0b4fc8e166dddccbcd13f9
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-08-16 17:50:42 -0400

    file streaming implemented

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ const gunzip = require('gunzip-file')
 
 const filePath = "./data.txt"
 
-console.log('app working!')
-
 // unzipps the file
-gunzip(filePath + '.gz', filePath, () => {
-  console.log('gunzip done!')
+// gunzip(filePath + '.gz', filePath, () => {
+//   console.log('gunzip done!')
+// })
+
+const instream = fs.createReadStream('data.txt')
+const outstream = new stream()
+const rl = readline.createInterface(instream, outstream)
+
+let lineCount = 0
+
+
+rl.on('line', (line) => {
+  lineCount += 1
+})
+
+
+rl.on('close', () => {
+  console.log(lineCount)
 })
