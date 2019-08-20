4421ae4d588d3b954dc6b23ee40b28e04ddb93ff
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-08-19 23:32:25 -0400

    write results to file

--- app.js
+++ app.js
@@ -61,49 +61,71 @@ decompressFile.then((result) => {
     }
   })
 
+  let logger = fs.createWriteStream('results.txt', {
+    flags: 'a' // 'a' means appending (old data will be preserved)
+  })
 
   rl.on('close', () => {
+    fs.writeFile('results.txt', '', function(){console.log('file reset')})
+
+    logger.write('Actor Name Program Results\n\n')
+
     console.log("o----------------------------o")
-    console.log("| Actor Name Program Results | ")
+    console.log("| Actor Name Program Results |")
     console.log("o----------------------------o")
     console.log()
 
     // 1. The unique count of full names (i.e. duplicates are counted only once)
     console.log(`Unique full names: ${Object.keys(fullNames).length}`)
+    logger.write(`Unique full names: ${Object.keys(fullNames).length}\n`)
 
     // 2. The unique count of last names
     console.log(`Unique last names: ${Object.keys(lastNames).length}`)
+    logger.write(`Unique last names: ${Object.keys(lastNames).length}\n`)
 
     // 3. The unique count of first names
     console.log(`Unique first names: ${Object.keys(firstNames).length}`)
+    logger.write(`Unique first names: ${Object.keys(firstNames).length}\n`)
     console.log()
+    logger.write('\n')
 
     // 4. The ten most common last names (the names and number of occurrences)
-    console.log("The ten most common last names (# of occurances):")
+    console.log("The ten most common last names (# of occurrences):")
+    logger.write("The ten most common last names (# of occurrences):\n")
     topLastNames.forEach(name => {
       console.log(` * ${name.name} (${name.count})`)
+      logger.write(` * ${name.name} (${name.count})\n`)
     })
     console.log()
+    logger.write('\n')
 
     // 5. The ten most common first names (the names and number of occurrences)
-    console.log("The ten most common first names (# of occurances):")
+    console.log("The ten most common first names (# of occurrences):")
+    logger.write('The ten most common first names (# of occurrences):\n')
     topFirstNames.forEach(name => {
       console.log(` * ${name.name} (${name.count})`)
+      logger.write(` * ${name.name} (${name.count})\n`)
     })
     console.log()
+    logger.write('\n')
 
     // 6. A list of 25 completely unique names
     console.log(`First ${N} unique names:`)
+    logger.write(`First ${N} unique names:\n`)
     uniqueNames.forEach(name => {
       console.log(` * ${name.first} ${name.last}`)
+      logger.write(` * ${name.first} ${name.last}\n`)
     })
     console.log()
+    logger.write('\n')
 
     // 7. A list of 25 modified names
     console.log(`First ${N} unique, modified names:`)
+    logger.write(`First ${N} unique, modified names:\n`)
     const modifiedNames = modifyNames(uniqueNames)
     modifiedNames.forEach(name => {
       console.log(` * ${name.first} ${name.last}`)
+      logger.write(` * ${name.first} ${name.last}\n`)
     })
 
   })
